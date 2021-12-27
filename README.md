0d488d53cf2038e4bc777aa15c43f2e1692b2d76
bufsnake <bufsnake@qq.com>
2021-12-27 09:55:43 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,15 @@
+## usort
+
+> 排序URL
+
+## 使用
+
+- cat urlstr-list|usort
+  
+- usort -target urlstr-list
+  
+- usort -target-list urlstr-list
+
+![image-20211227095138563](.images/image-20211227095138563.png)
+
+![image-20211227095206629](.images/image-20211227095206629.png)
---
+++ main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"net/url"
+	"os"
+	"sort"
+	"strings"
+)
+
+// 排序URL
+// 获取全部路径，同时获取路径协议-http/https
+// 获取全部路径数组，排序数组，输出
+func main() {
+	target := flag.String("target", "", "")
+	targets := flag.String("target-list", "", "")
+	flag.Parse()
+	probes := make(map[string]bool)
+	file := make([]byte, 0)
+	var err error
+	if *target != "" {
+		probes[*target] = true
+	} else if *targets != "" {
+		file, err = os.ReadFile(*targets)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		urls := strings.Split(string(file), "\n")
+		for i := 0; i < len(urls); i++ {
+			urls[i] = strings.Trim(urls[i], " \t\r")
+			if urls[i] == "" {
+				continue
+			}
+			probes[urls[i]] = true
+		}
+	} else {
+		sc := bufio.NewScanner(os.Stdin)
+		for sc.Scan() {
+			probes[sc.Text()] = true
+		}
+		if err = sc.Err(); err != nil {
+			fmt.Println("failed to read input:", err)
+			return
+		}
+	}
+	if len(probes) == 0 {
+		flag.Usage()
+		return
+	}
+	hostpath_schemas := make(map[string][]string)
+	hostpaths := make([]string, 0)
+	for urlstr, _ := range probes {
+		var schema, hostpath = "", ""
+		schema, hostpath, err = urlparse(urlstr)
+		if err != nil {
+			_, _ = fmt.Fprintf(os.Stderr, "%s %s", urlstr, err)
+			continue
+		}
+		if hostpath == "" {
+			continue
+		}
+		if _, ok := hostpath_schemas[hostpath]; !ok {
+			hostpath_schemas[hostpath] = make([]string, 0)
+			hostpaths = append(hostpaths, hostpath)
+		}
+		hostpath_schemas[hostpath] = append(hostpath_schemas[hostpath], schema)
+	}
+	sort.Strings(hostpaths)
+	for i := 0; i < len(hostpaths); i++ {
+		sort.Strings(hostpath_schemas[hostpaths[i]])
+		for j := 0; j < len(hostpath_schemas[hostpaths[i]]); j++ {
+			fmt.Println(hostpath_schemas[hostpaths[i]][j] + "://" + hostpaths[i])
+		}
+	}
+}
+
+func urlparse(urlstr string) (schema string, hostpath string, err error) {
+	var parse *url.URL
+	parse, err = url.Parse(urlstr)
+	if err != nil {
+		return
+	}
+	schema = parse.Scheme
+	hostpath = parse.Host + parse.Path
+	if parse.RawQuery != "" {
+		hostpath += "?" + parse.RawQuery
+	}
+	return
+}
